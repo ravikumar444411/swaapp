c7dcb57d79233173a55e7ce9f4571c3b6d8c8d6a
ravikumar444411 <69462285+ravikumar444411@users.noreply.github.com>
2021-02-21 15:28:15 +0530

    Add files via upload

---
+++ static/js/9.baad0caa.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[9],{745:function(e,t,n){"use strict";n.r(t);var r=n(685),a=n(164),c=n(632);t.default=Object(a.b)((function(e){return{data:e.getTodaySession}}),(function(e){return{getTodaySessionHandler:function(t){return e(Object(c.b)(t))}}}))(r.a)}}]);
+//# sourceMappingURL=9.baad0caa.chunk.js.map
